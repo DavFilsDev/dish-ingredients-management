9e51fbc7f38a15171894bf0d22864b839bd00cf3
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-28 21:42:28 +0300

    feat(init-structure): add basics structure by default by maven

---
+++ src/main/java/org/zenith/dishIngredients/DishIngredientsManagementApplication.java
@@ -0,0 +1,13 @@
+package org.zenith.dishIngredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishIngredientsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishIngredientsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zenith/dishIngredients/DishIngredientsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zenith.dishIngredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishIngredientsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
