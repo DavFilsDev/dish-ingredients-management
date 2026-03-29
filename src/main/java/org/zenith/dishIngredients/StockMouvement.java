6e74a758522b3452ab52b96dcf1ad288a978bed6
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-29 07:10:20 +0300

    feat(model): add class StockMouvement

---
+++ src/main/java/org/zenith/dishIngredients/StockMouvement.java
@@ -0,0 +1,34 @@
+package org.zenith.dishIngredients;
+
+import java.time.Instant;
+
+public class StockMouvement {
+
+    private final int id;
+    private final StockValue value;
+    private final MovementTypeEnum type;
+    private final Instant creationDateTime;
+
+    public StockMouvement(int id, StockValue value, MovementTypeEnum type, Instant creationDateTime) {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+        this.creationDateTime = creationDateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public StockValue getValue() {
+        return value;
+    }
+
+    public MovementTypeEnum getType() {
+        return type;
+    }
+
+    public Instant getCreationDateTime() {
+        return creationDateTime;
+    }
+}
