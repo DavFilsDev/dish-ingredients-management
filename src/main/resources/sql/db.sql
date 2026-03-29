464991f1a9447732503f4ba1cc61c8d30c648c4b
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-29 06:22:58 +0300

    refactor(script): rename the db.sql into 01_db.sql for easy track of all the scripts

--- src/main/resources/sql/db.sql
+++
@@ -1,8 +0,0 @@
-create database mini_dish_db;
-create user mini_dish_db_manager with password 'admin_mini_dish_db';
-
-grant connect on database mini_dish_db to mini_dish_db_manager;
-grant create on schema public to mini_dish_db_manager;
-
-alter default privileges in schema public grant select, insert, update, delete on tables to mini_dish_db_manager;
-alter default privileges in schema public grant usage, select, update on sequences to mini_dish_db_manager;
